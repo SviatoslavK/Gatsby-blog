aa3da76a181df64529927db0e788c546580da1ce
Sviatoslav Koloda <sviatoslav.koloda@cpamatica.io>
2018-01-04 16:53:54 +0200

    test deploy

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,6 +2,7 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
+  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/home/sviatoslav/project/Gatsby-blog/.cache/dev-404-page.js"),
   "component---src-templates-blog-post-js": require("gatsby-module-loader?name=component---src-templates-blog-post-js!/home/sviatoslav/project/Gatsby-blog/src/templates/blog-post.js"),
   "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/home/sviatoslav/project/Gatsby-blog/src/pages/404.js"),
   "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/home/sviatoslav/project/Gatsby-blog/src/pages/index.js"),
@@ -9,6 +10,8 @@ exports.components = {
 }
 
 exports.json = {
+  "layout-index.json": require("gatsby-module-loader?name=path---!/home/sviatoslav/project/Gatsby-blog/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/home/sviatoslav/project/Gatsby-blog/.cache/json/dev-404-page.json"),
   "layout-index.json": require("gatsby-module-loader?name=path---!/home/sviatoslav/project/Gatsby-blog/.cache/json/layout-index.json"),
   "react-gatsby-blog.json": require("gatsby-module-loader?name=path---react-gatsby-blog!/home/sviatoslav/project/Gatsby-blog/.cache/json/react-gatsby-blog.json"),
   "layout-index.json": require("gatsby-module-loader?name=path---!/home/sviatoslav/project/Gatsby-blog/.cache/json/layout-index.json"),
@@ -18,7 +21,9 @@ exports.json = {
   "layout-index.json": require("gatsby-module-loader?name=path---!/home/sviatoslav/project/Gatsby-blog/.cache/json/layout-index.json"),
   "page-2.json": require("gatsby-module-loader?name=path---page-2!/home/sviatoslav/project/Gatsby-blog/.cache/json/page-2.json"),
   "layout-index.json": require("gatsby-module-loader?name=path---!/home/sviatoslav/project/Gatsby-blog/.cache/json/layout-index.json"),
-  "404-html.json": require("gatsby-module-loader?name=path---404-html!/home/sviatoslav/project/Gatsby-blog/.cache/json/404-html.json")
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/home/sviatoslav/project/Gatsby-blog/.cache/json/404-html.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/home/sviatoslav/project/Gatsby-blog/.cache/json/layout-index.json"),
+  "react.json": require("gatsby-module-loader?name=path---react!/home/sviatoslav/project/Gatsby-blog/.cache/json/react.json")
 }
 
 exports.layouts = {
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,6 +7,7 @@ exports.layouts = {
 }
 
 exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/home/sviatoslav/project/Gatsby-blog/.cache/dev-404-page.js")),
   "component---src-templates-blog-post-js": preferDefault(require("/home/sviatoslav/project/Gatsby-blog/src/templates/blog-post.js")),
   "component---src-pages-404-js": preferDefault(require("/home/sviatoslav/project/Gatsby-blog/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/home/sviatoslav/project/Gatsby-blog/src/pages/index.js")),
@@ -14,6 +15,8 @@ exports.components = {
 }
 
 exports.json = {
+  "layout-index.json": require("/home/sviatoslav/project/Gatsby-blog/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("/home/sviatoslav/project/Gatsby-blog/.cache/json/dev-404-page.json"),
   "layout-index.json": require("/home/sviatoslav/project/Gatsby-blog/.cache/json/layout-index.json"),
   "react-gatsby-blog.json": require("/home/sviatoslav/project/Gatsby-blog/.cache/json/react-gatsby-blog.json"),
   "layout-index.json": require("/home/sviatoslav/project/Gatsby-blog/.cache/json/layout-index.json"),
@@ -23,5 +26,7 @@ exports.json = {
   "layout-index.json": require("/home/sviatoslav/project/Gatsby-blog/.cache/json/layout-index.json"),
   "page-2.json": require("/home/sviatoslav/project/Gatsby-blog/.cache/json/page-2.json"),
   "layout-index.json": require("/home/sviatoslav/project/Gatsby-blog/.cache/json/layout-index.json"),
-  "404-html.json": require("/home/sviatoslav/project/Gatsby-blog/.cache/json/404-html.json")
+  "404-html.json": require("/home/sviatoslav/project/Gatsby-blog/.cache/json/404-html.json"),
+  "layout-index.json": require("/home/sviatoslav/project/Gatsby-blog/.cache/json/layout-index.json"),
+  "react.json": require("/home/sviatoslav/project/Gatsby-blog/.cache/json/react.json")
 }
